b8aa057e0279ce2c2d14db08fe2e860d891597ed
mgd <mgd@posteo.net>
2024-11-17 14:05:25 +0000

    Initial Commit

--- src/main/java/com/mg/springweatherapp/SpringWeatherAppApplication.java
+++ src/main/java/com/mg/springweatherapp/SpringWeatherAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWeatherAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWeatherAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeatherAppApplication.class, args);
+    }
 
 }
--- src/test/java/com/mg/springweatherapp/SpringWeatherAppApplicationTests.java
+++ src/test/java/com/mg/springweatherapp/SpringWeatherAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringWeatherAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
