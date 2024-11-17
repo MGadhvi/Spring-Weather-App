7cd187b09c6ac9c80b8350ffd2a201fc2d5cbf90
mgd <mgd@posteo.net>
2024-11-17 14:14:48 +0000

    Added weather service

--- src/main/java/com/mg/springweatherapp/services/WeatherService.java
+++ src/main/java/com/mg/springweatherapp/services/WeatherService.java
@@ -1,4 +1,22 @@
 package com.mg.springweatherapp.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class WeatherService {
+    @Value("${openweather.api.key}")
+    private String apiKey;
+
+    @Value("${openweather.api.url}")
+    private String apiUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public String getWeather(String city) {
+        String url = String.format("%s?q=%s&appid=%s&units=metric", apiUrl, city, apiKey);
+        return restTemplate.getForObject(url, String.class);
+    }
 }
+
