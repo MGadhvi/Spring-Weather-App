3845a265a94010096d9d6a06191fa98f790d5efb
mgd <mgd@posteo.net>
2024-11-17 14:15:05 +0000

    Added weather controller

--- src/main/java/com/mg/springweatherapp/controller/WeatherController.java
+++ src/main/java/com/mg/springweatherapp/controller/WeatherController.java
@@ -1,4 +1,31 @@
 package com.mg.springweatherapp.controller;
 
+import com.mg.springweatherapp.services.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/weather")
+    public String getWeather(@RequestParam("city") String city, Model model) {
+        String weatherData = weatherService.getWeather(city);
+        model.addAttribute("weatherData", weatherData);
+        return "weather";
+    }
 }
